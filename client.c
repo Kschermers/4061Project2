50b6cbdf45a068fde060a459af34ec984efa1f71
Samuel Ball <ballx188@umn.edu>
2018-11-01 22:12:14 -0500

    Added DEBUG prints to determine runtime errors

--- client.c
+++ client.c
@@ -33,12 +33,15 @@ void handle_signals(int sig_num){signalled = 1;}
 /* -------------------------Main function for the client ----------------------*/
 void main(int argc, char * argv[]) {
 	int pipe_to_user[2], pipe_to_server[2];
+	int flags;
 
+	printf("DEBUG: connecting to server...\n\n");
 	// You will need to get user name as a parameter, argv[1].
 	if(connect_to_server("ok", argv[1], pipe_to_user, pipe_to_server) == -1) {
 		exit(-1);
 	}
-	
+	printf("DEBUG: connection success!\n\n");
+
 	/* -------------- YOUR CODE STARTS HERE -----------------------------------*/
 	// signal handling
     struct sigaction my_sa = {};
@@ -54,31 +57,38 @@ void main(int argc, char * argv[]) {
     //Should we close these here?? Cause of the comm methods creating the pipe thing..
 	close(pipe_to_user[1]);
 	close(pipe_to_server[0]);
-
+	printf("DEBUG: Closed pipes!\n\n");
+	
 	// make reading from server nonblocking
-	fcntl(pipe_to_user[0], F_SETFL, O_NONBLOCK);
+	flags = fcntl(pipe_to_user[0], F_GETFL, 0);
+	fcntl(pipe_to_user[0], F_SETFL, flags | O_NONBLOCK);
+	printf("DEBUG: Read from server set to nonblocking!\n\n");
 
 	// make stdin nonblocking
-    int flags = fcntl(0, F_GETFL, 0); /* get current file status flags */
+    flags = fcntl(0, F_GETFL, 0); /* get current file status flags */
     flags |= O_NONBLOCK;    /* turn off blocking flag */
     fcntl(0, F_SETFL,0);    /* set up non-blocking read */
 
-
+	printf("DEBUG: About to enter user-process loop\n\n");
 	while(!signalled){
 		// poll pipe retrieved and print it to stdout
+		printf("DEBUG: polling pipe\n\n");
 		int bytesRead = read(pipe_to_user[0], buf_recieve, MAX_MSG);
         if(bytesRead > 0){
+			printf("DEBUG: >0 bytes read from pipe\n\n");
             write(1, buf_recieve, MAX_MSG);
         }
 		memset(buf_recieve, '\0', MAX_MSG);
 
 		// Poll stdin (input from the terminal) and send it to server (child process) via pipe
-      
+      	printf("DEBUG: polling stdin...\n\n");
 		int bytesRead2 = read(0, buf_send, MAX_MSG);
         if(bytesRead2 > 0){
+			printf("DEBUG: >0 bytes read from stdin\n\n");
             write(pipe_to_server[1], buf_send, MAX_MSG);
         }
 		memset(buf_send, '\0', MAX_MSG);
+		printf("DEBUG: End of user-process loop\n\n");
 	}	
 	/* -------------- YOUR CODE ENDS HERE -----------------------------------*/
 }
